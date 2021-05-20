238ecfa9e2f2f45f788fa48839d4c0aa5a660008
joannakwiat <62302596+joannakwiat@users.noreply.github.com>
2021-05-20 15:17:27 +0200

    Zrobione menu

---
+++ Dogmon 3D/Assets/Menu.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+	//index 0 = menu
+	//index 1 = gra (lvl 1)
+	//przeskok między scenami dzięki inkrementacji indexu
+    public void PlayGame(){
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void QuitGame(){
+        Application.Quit();
+    }
+}
