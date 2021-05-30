505cd79a888b65a43b731929df03a71758070d77
Milena Kierys <melaka.mk@gmail.com>
2021-05-30 17:04:38 +0200

    sprzatansko

---
+++ Dogmon 3D/Assets/Scripts/BoneRotation.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoneRotation : MonoBehaviour
+{
+    public GameObject kosc; 
+    float speed = 70.0f; 
+    
+    void Update(){
+        kosc.transform.Rotate(Vector3.up * speed * Time.deltaTime);        
+    }
+}
---
+++ Dogmon 3D/Assets/Scripts/enemyFollow.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemyFollow : MonoBehaviour
+{
+    public Transform target;
+    public float speed = 4f;
+    private Rigidbody rig;
+    public PlayerMovement player;
+    
+    private void Start(){
+        rig = GetComponent<Rigidbody>();
+        player = FindObjectOfType<PlayerMovement> ();
+    }
+    
+    private void Update(){
+        var pos = Vector3.MoveTowards(transform.position, target.position, speed * Time.fixedDeltaTime);
+        rig.MovePosition(pos);
+        transform.LookAt(target);
+    }
+
+    private void OnCollisionEnter(Collision collision){        
+        if (collision.gameObject.name.Contains("Player")){
+            player.energy (-1);
+        }
+    }
+}
